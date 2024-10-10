948124a676f431b4855880f3990a8e4d2fd4202e
stefanosilva94 <stefano.silva1994@gmail.com>
2024-10-10 11:00:47 +0100

    Update home widgets to display totw count and promo count Add basic template for my_packs page

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('api/create-distribution-rating-chart/', views.create_distribution_rating_chart, name='create_distribution_rating_chart'),
+    path('my-packs/', views.my_packs, name='my_packs'),
     ]
 
--- dashboards/views.py
+++ dashboards/views.py
@@ -4,11 +4,12 @@ from .charts.chart_utils import create_time_query_filter
 from django.http import JsonResponse
 from .charts import charts
 import json
+from django.contrib.auth.decorators import login_required
+
 
 
 def home(request):
     with connection.cursor() as cursor:
-        
         
         cursor.execute("SELECT COUNT(DISTINCT(pack_id)) FROM packed_items")
         total_packs = cursor.fetchone()[0]
@@ -25,12 +26,19 @@ def home(request):
         # Flatten the list of tuples
         pack_names = [name[0] for name in pack_names] 
         pick_names = [name[0] for name in pick_names] 
-  
+        
+        pack_counts = get_counts_of_card_types('packed_items')
+        pick_counts = get_counts_of_card_types('player_picks')
+        
+        totw_count = pack_counts.get('totw_count', 0) + pick_counts.get('totw_count', 0)
+        promo_count = pack_counts.get('promo_count', 0) + pick_counts.get('promo_count', 0)
     return render(request, 'dashboards/home.html', 
                   {'total_packs': total_packs,
                    'total_picks': total_picks,
                    'pack_names': pack_names,
                    'pick_names': pick_names,
+                   'total_totws': totw_count,
+                   'total_promos': promo_count
                    })
 
 
@@ -68,17 +76,68 @@ def create_distribution_rating_chart(request):
 
 
 def my_packs(request):
-    return render(request, 'dashboards/my_packs.html')
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        
+        with connection.cursor() as cursor:
+            cursor.execute(count_query, ['user_id'])
+        
+        
+        
+        return render(request, 'dashboards/my_packs.html', {"title": "My Packs"})
+    else:
+        return render(request, 'dashboards/please_log_in.html', {"title": "Log In Needed"})
 
 
 def privacy_policy(request):
     return render(request, 'dashboards/privacy_policy.html')
 
 
-def get_count_of_card_type(raretype):
+def get_counts_of_card_types(table_name, pack_name=None, user_id=None):
     """
     This will work out the count of a certain card type. 
     E.g. TOTW - raretype = 3
-     
     """
-    pass
+    
+    # Validate that the table name is safe (optional step)
+    allowed_tables = ['packed_items', 'player_picks']  # List of allowed table names
+    if table_name not in allowed_tables:
+        raise ValueError("Invalid table name")
+    
+    count_query = f"""
+        SELECT
+            COUNT(CASE WHEN CAST(raretype AS int) NOT IN (0, 1, 3, 50, 51, 52, 1004) THEN 1 END) AS promo_count,
+            COUNT(CASE WHEN CAST(raretype AS int) = 3 THEN 1 END) AS totw_count,
+            COUNT(CASE WHEN CAST(raretype AS int) IN (72, 161) THEN 1 END) AS hero_count,
+            COUNT(CASE WHEN CAST(raretype AS int) = 12 THEN 1 END) AS icon_count,
+            COUNT(CASE WHEN CAST(raretype AS int) = 1 THEN 1 END) AS normal_count
+        FROM 
+            {table_name}
+    """
+    
+    filters = []
+    params = []
+    
+    if pack_name:
+        filters.append("pack_name = %s")
+        params.append(pack_name)
+    
+    if user_id:
+        filters.append("user_id = %s")
+        params.append(user_id)
+    
+    if filters:
+        count_query += " WHERE " + " AND ".join(filters)
+    
+    # Execute the query
+    with connection.cursor() as cursor:
+        cursor.execute(count_query, params)
+        counts = cursor.fetchone()
+
+    return { 
+            'promo_count': counts[0],
+            'totw_count': counts[1],
+            'hero_count': counts[2],
+            'icon_count': counts[3]
+            }
+    
